2695ed0fa129bf76926da1f24b738c0a78b7383d
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-10-25 12:12:18 -0500

    can now add/remove edges between nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import { useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import {
   ReactFlow,
   Background,
   Controls,
   MiniMap,
   addEdge,
-  useNodesState,
-  useEdgesState,
+  // useNodesState,
+  // useEdgesState,
+  applyEdgeChanges,
+  applyNodeChanges,
   type OnConnect,
+  NodeChange,
+  Edge,
+  EdgeChange,
 } from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
 
 import { initialNodes, nodeTypes } from './nodes';
 import { initialEdges, edgeTypes } from './edges';
+import { AppNode } from './nodes/types';
+
 
 export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  // This version allows us to add and remove edges
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+  
+  const onNodesChange = useCallback(
+    (changes: NodeChange<AppNode>[]) => setNodes((nodes) => applyNodeChanges(changes, nodes)),
+    [],
+  );
+  
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange<Edge>[]) => setEdges((edges) => applyEdgeChanges(changes, edges)),
+    [],
+  );
+
   const onConnect: OnConnect = useCallback(
     (connection) => setEdges((edges) => addEdge(connection, edges)),
-    [setEdges]
+    [],
   );
 
   return (
@@ -38,5 +57,32 @@ export default function App() {
       <MiniMap />
       <Controls />
     </ReactFlow>
-  );
+  )
 }
+
+// Default app code
+// export default function App() {
+//   const [nodes, , onNodesChange] = useNodesState(initialNodes);
+//   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+//   const onConnect: OnConnect = useCallback(
+//     (connection) => setEdges((edges) => addEdge(connection, edges)),
+//     [setEdges]
+//   );
+
+//   return (
+//     <ReactFlow
+//       nodes={nodes}
+//       nodeTypes={nodeTypes}
+//       onNodesChange={onNodesChange}
+//       edges={edges}
+//       edgeTypes={edgeTypes}
+//       onEdgesChange={onEdgesChange}
+//       onConnect={onConnect}
+//       fitView
+//     >
+//       <Background />
+//       <MiniMap />
+//       <Controls />
+//     </ReactFlow>
+//   );
+// }
